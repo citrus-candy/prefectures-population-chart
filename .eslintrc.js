f21f6a6232be1b8385519b8aeda610fdbad3ee7f
citrus-candy <kyuuri11013@gmail.com>
2021-11-02 22:03:27 +0900

    [fix] eslintとprettierの環境をvscode用に調整

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,14 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true,
+    node: true
   },
   extends: [
     '@nuxtjs/eslint-config-typescript',
-    'plugin:prettier/recommended',
     'plugin:nuxt/recommended',
+    'eslint-config-prettier'
   ],
   plugins: [],
   // add your custom rules here
-  rules: {},
+  rules: {}
 }
