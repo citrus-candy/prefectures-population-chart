66fc02a75057eacde9b8ca341aba90bbad5f8949
citrus-candy <kyuuri11013@gmail.com>
2021-11-09 00:21:04 +0900

    [change] 名前を変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,9 +4,9 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'resas-graph-spa',
+    title: 'Prefectures Population Chart',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'ja'
     },
     meta: [
       { charset: 'utf-8' },
